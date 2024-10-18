e71061922572af22348801416a9d51806080db72
Puxuan Wang <realharrison034@gmail.com>
2024-10-18 10:54:14 -0400

    fixed cors

--- ThriftU_Backend/app.py
+++ ThriftU_Backend/app.py
@@ -7,7 +7,9 @@ from EmailSender import send_verification_email
 import jwt
 import datetime
 import os
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 # Database connection information
 # DB_HOST = "localhost"
